93c228ce828d061077931217d0b909e697b0a506
kkirby16 <kkirby16@my.whitworth.edu>
2020-01-17 14:00:27 -0800

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,85 +1,83 @@
 require 'pp'
 require 'pry'
 
-def map_to_negativize(source_array) #map
-  counter = 0 
-  negative_array = []
-  while counter < source_array.length do 
-    negative_array << source_array[counter] * -1  
-    counter += 1
+def map_to_negativize(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.size do 
+  new_array << source_array[i] * -1 
+  i += 1
   end
-negative_array
-end 
+  new_array
 
-def map_to_no_change(source_array) 
-  counter = 0 
-  unchanged_array = [] 
-  while counter < source_array.length do 
-    unchanged_array << source_array[counter]
-    counter += 1 
+end
+
+def map_to_no_change(source_array)
+  i = 0
+  while i < source_array.size do 
+  source_array[i]
+  i += 1 
   end 
-  unchanged_array
-end 
+  source_array
+end
 
 def map_to_double(source_array)
-  counter = 0 
-  multiplied_array = [] 
-  while counter < source_array.length do 
-    multiplied_array << source_array[counter] * 2
-    counter += 1 
+  doubled_array = []
+  i = 0 
+  while i < source_array.length do 
+  doubled_array << source_array[i] * 2 
+  i += 1 
   end 
-  multiplied_array
-end 
+  doubled_array
+end
 
-def map_to_square(source_array) 
-  counter = 0 
-  squared_array = [] 
-  while counter < source_array.length do 
-    squared_array << source_array[counter] ** 2 
-    counter += 1 
-  end  
+def map_to_square(source_array)
+  squared_array = []
+  i = 0 
+  while i < source_array.length do 
+  squared_array << source_array[i] ** 2
+  i += 1 
+  end 
   squared_array
-end 
+end
 
 def reduce_to_total(source_array, starting_point=0)
   total = starting_point
-  counter = 0
-  while counter < source_array.length do 
-    total += source_array[counter]
-    counter += 1 
-    #binding.pry
+  i = 0
+  while i < source_array.length do 
+  total += source_array[i]
+  i += 1 
   end 
-  total
-end  
+total 
+end
 
 def reduce_to_all_true(source_array)
- counter = 0 
- while counter < source_array.length do 
-  if source_array[counter] == false 
-    return false  
-  end 
-  counter += 1
- end 
- true
-
-end 
+  i = 0 
+  while i < source_array.length do 
+    if source_array.all? == true 
+      return true
+    else  
+      return false
+    end
+    i += 1
+  end
+end
 
 def reduce_to_any_true(source_array)
-  counter = 0 
-  while counter < source_array.length do
-    if source_array[counter] == true
-      return true 
-    end 
-    counter += 1 
-  end 
-  false
-end  
+  i = 0 
+  while i < source_array.length do 
+    if source_array.any? == true 
+    return true 
+    else 
+    return false
+    end
+  i += 1  
+  end
+end
 
 
- 
 
 
- 
 
 
-    
+
